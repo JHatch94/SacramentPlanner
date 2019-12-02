09f454d72cde719c8a0f90965c2db66c10a1856d
Arthur Figueiredo <fig16009@gmail.com>
2019-12-02 14:57:26 -0700

    Created Hymn

---
+++ SacramentPlanner/Models/Hymn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlanner.Models
+{
+    public class Hymn
+    {
+        public int HymnId { get; set; }
+        public int HymnNumber { get; set; }
+        public string HymnName { get; set; }
+    }
+}
