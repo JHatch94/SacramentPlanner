8587f35f4091c85c86fc8d1e2d0d6b5b85d569a2
JHatch94 <35378910+JHatch94@users.noreply.github.com>
2019-12-02 14:55:15 -0700

    Added two classes

---
+++ SacramentPlanner/Models/Bishopric.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlanner.Models
+{
+    public class Bishopric
+    {
+        public int BishopricId { get; set; }
+        public String BishopricPosition { get; set; }
+        public String BishopricName { get; set; }
+    }
+}
---
+++ SacramentPlanner/Models/Topics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlanner.Models
+{
+    public class Topics
+    {
+        public int TopicsId { get; set; }
+        public int TopicName { get; set; }
+    }
+}
