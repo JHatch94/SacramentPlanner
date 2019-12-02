40e9c00d06065b9acc8d50076a5cf1c5665ef12e
JHatch94 <35378910+JHatch94@users.noreply.github.com>
2019-12-02 14:48:04 -0700

    Cool cool cool

---
+++ SacramentPlanner/Models/Speaker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlanner.Models
+{
+    public class Speaker
+    {
+        public int SpeakerId { get; set; }
+        public int MeetingId { get; set; }
+        public String SpeakerName { get; set; }
+        public String SpeakerName { get; set; }
+    }
+}
