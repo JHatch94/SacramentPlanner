6b819acc5b46224823563b3096d997b5db30dd9d
JHatch94 <35378910+JHatch94@users.noreply.github.com>
2019-12-07 11:43:43 -0700

    Added Meeting Speaker

---
+++ SacramentPlanner/Models/MeetingSpeaker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentPlanner.Models
+{
+    public class MeetingSpeaker
+    {
+        public int MeetingSpeakerId { get; set; }
+        public Meeting meeting { get; set; }
+        public int MeetingId { get; set; }
+        public Speaker speaker { get; set; }
+        public int SpeakerId { get; set; }
+    }
+}
