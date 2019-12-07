3367583f23b5af98ef20b4dae52c58809a9df247
Arthur Figueiredo <fig16009@gmail.com>
2019-12-07 12:22:00 -0700

    Adding Validation

--- SacramentPlanner/Models/Meeting.cs
+++ SacramentPlanner/Models/Meeting.cs
@@ -9,44 +9,60 @@ namespace SacramentPlanner.Models
     public class Meeting
     {
         public int MeetingId { get; set; }
+
         [DataType(DataType.Date)]
+        [Required]
         public DateTime Date { get; set; }
+
+        [Required]
         public string Presiding { get; set; }
 
         [Display(Name = "Opening Hymn")]
+        [Required]
         public string OpeningHymn { get; set; }
 
         [Display(Name = "Hymn #")]
+        [Required]
         public int OpeningHymnNumber { get; set; }
 
         [Display(Name = "Opening Prayer")]
+        [Required]
         public string OpeningPrayer { get; set; }
 
         [Display(Name = "Sacrament Hymn")]
+        [Required]
         public string SacramentHymn { get; set; }
 
         [Display(Name = "Hymn #")]
+        [Required]
+        [Range(1, 341)]
         public int SacramentHymnNumber { get; set; }
 
         [Display(Name = "Speaker Name")]
+        [Required]
         public Speaker Speaker { get; set; }
 
         //[Display(Name = "Speakers")]
         //public ICollection<Speaker> Speakers { get; set; }
 
         [Display(Name = "Intermediate Hymn (Optional)")]
-        public string IntermediateHymn { get; set; }
+        public string? IntermediateHymn { get; set; }
 
         [Display(Name = "Hymn # (Optional)")]
-        public int IntermediateHymnNumber { get; set; }
+        [Range(1, 341)]
+        public int? IntermediateHymnNumber { get; set; }
 
         [Display(Name = "Closing Hymn")]
+        [Required]
         public string ClosingHymn { get; set; }
 
         [Display(Name = "Hymn #")]
+        [Required]
+        [Range(1, 341)]
         public int ClosingHymnNumber { get; set; }
 
         [Display(Name = "Closing Prayer")]
+        [Required]
         public string ClosingPrayer { get; set; }
     }
 }
